556b6e0ce33bc7ccdfda5242207881a40fe2eb75
Max S <>
2024-12-07 09:17:21 +0100

    Changed all global variables to class context variables.

--- cbpi4-pressure-i2c/__init__.py
+++ cbpi4-pressure-i2c/__init__.py
@@ -19,25 +19,31 @@ import adafruit_tca9548a
 
 logger = logging.getLogger(__name__)
 
-
-
 @parameters([Property.Select(label="Socket", options=[0,1,2,3,4,5,6,7], description="Sensor socket"),
-             Property.Select(label="Unit", options=["Bar","PSI"], description="Pressure Unit"),
              Property.Number(label="Max PSI",configurable = True, default_value = 80, description="Sensor Max PSI (Default is 80)"),
              Property.Number(label="Offset",configurable = True, default_value = 0, description="Sensor Offset (Default is 0)"),
              Property.Select(label="Interval", options=[1,5,10,30,60], description="Interval in Seconds")])
 class PressureSensori2c(CBPiSensor):
-    
+    scale = None
+    calc_offset = None
+    chan = None
+    foo = 23
+    interval = 1
+    offset = 0
+    unit = "kPa"
+
     def __init__(self, cbpi, id, props):
         super(PressureSensori2c, self).__init__(cbpi, id, props)
         print("Init Pressure Sensor i2c Start")
-        global scale, calc_offset, chan
+        
         self.value = 0
         offset2 = 0.527
         voltage_min = 0.527
         voltage_max = 4.5
 
         socket_nr = int(self.props.get("Socket", 0))
+        self.foo = socket_nr
+        bar = socket_nr
 
         analog_pin = socket_nr
         ads_chip = 0
@@ -46,54 +52,46 @@ class PressureSensori2c(CBPiSensor):
             ads_chip = 1
 
         psi_max = int(self.props.get("Max PSI", 80))
+        self.interval = int(self.props.get("Interval",1))
+        self.offset = float(self.props.get("Offset",0))
+        self.unit = self.cbpi.config.get("PRESSURE_UNIT", "kPa")
+        if self.unit is None or self.unit == "" or not self.unit:
+            self.unit = "kPa"
+            self.cbpi.notify("Pressure Sensor Init Problem","Cant read config value: PRESSURE_UNIT. Unit set to {}".format(self.unit), NotificationType.WARNING)
 
-        scale = psi_max/(voltage_max - voltage_min)
-        #print(scale)
-        t = voltage_max * scale
-        #print(t)
-        calc_offset = psi_max - t
-        #print(offset)
+        self.scale = psi_max/(voltage_max - voltage_min)
+        t = voltage_max * self.scale
+        self.calc_offset = psi_max - t
 
         i2c = busio.I2C(board.SCL, board.SDA)
 
         # Create the TCA9548A object and give it the I2C bus
         tca = adafruit_tca9548a.TCA9548A(i2c)
 
-        chan = None
-
         # Create the ADS object and specify the gain
         try:
             ads = ADS.ADS1115(tca[ads_chip])
             ads.gain = 1
-            chan = AnalogIn(ads, analog_pin)
+            self.chan = AnalogIn(ads, analog_pin)
         except Exception as e:
             self.cbpi.notify("Pressure Sensor Init Error","Cant read from input, ADS: {}, Pin: {}, Error: {}".format(ads_chip, analog_pin, e), NotificationType.ERROR)
             return
 
-        #ads = ADS.ADS1115(i2c)
-        #chan = AnalogIn(ads, ADS.P0)
         print("Init Pressure Sensor i2c Done")
 
     async def run(self):
         while self.running is True:
-            global chan, calc_offset, scale
-            interval = int(self.props.get("Interval",1))
-            offset = int(self.props.get("Offset",0))
-            unit = self.props.get("Unit","Bar")
-            #self.value = random.randint(0,50)
-            #self.push_update(self.value)
             
-            psi = (scale * chan.voltage) + calc_offset
+            psi = (self.scale * self.chan.voltage) + self.calc_offset
 
-            if unit == "PSI":
-                self.value = psi + offset
-            if unit == "Bar":
-                self.value = round(psi / 14.5038, 3) + offset
+            if self.unit == "PSI":
+                self.value = psi + self.offset
+            if self.unit == "kPa":
+                self.value = round(psi * 6.89476) + self.offset
             
             #print(f"MQ-135 Voltage: {chan.voltage}V , {chan.value}, {P}, {psi} PSI, {bar} BAR")
-            #print("RUN")
             self.push_update(self.value)
-            await asyncio.sleep(interval)
+            await asyncio.sleep(self.interval)
     
     def get_state(self):
         return dict(value=self.value)
