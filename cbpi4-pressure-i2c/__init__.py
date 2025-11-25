6b138686c037fd6cdec22bd1ad26a25e67ab5e06
Max S <>
2025-11-25 21:45:56 +0100

    Redo

--- cbpi4-pressure-i2c/__init__.py
+++ cbpi4-pressure-i2c/__init__.py
@@ -83,8 +83,7 @@ class PressureSensori2c(CBPiSensor):
         while self.running is True:
             
             try:
-                v = await self.chan.voltage
-                psi = (self.scale * v) + self.calc_offset
+                psi = (self.scale * self.chan.voltage) + self.calc_offset
                 if self.unit == "PSI":
                     self.value = psi + self.offset
                 if self.unit == "kPa":
