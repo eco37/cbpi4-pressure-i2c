c9857efae46f1010bf1ce66a14e170986131fde0
Max S <>
2025-12-17 13:10:07 +0100

    Changed version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cbpi4-pressure-i2c',
-      version='0.0.2',
+      version='0.1.0',
       description='Pressure Sensor i2c with tca9548A and two ADS1115',
       author='Max Sidenstjärna',
       author_email='',
