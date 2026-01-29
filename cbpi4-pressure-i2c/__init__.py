1f5c71c81a8549ada9c264db594b311b8a9a6993
Max S <>
2026-01-29 22:20:01 +0100

    test

--- cbpi4-pressure-i2c/__init__.py
+++ cbpi4-pressure-i2c/__init__.py
@@ -101,8 +101,8 @@ class PressureSensori2c(CBPiSensor):
             self.unit = "kPa"
             self.cbpi.notify("Pressure Sensor Init Problem","Cant read config value: PRESSURE_UNIT. Unit set to {}".format(self.unit), NotificationType.WARNING)
 
-        self.scale = psi_max/(voltage_max - voltage_min)
-        t = voltage_max * self.scale
+        self.scale = psi_max/(self.voltage_max - voltage_min)
+        t = self.voltage_max * self.scale
         self.calc_offset = psi_max - t
 
         i2c = busio.I2C(board.SCL, board.SDA)
